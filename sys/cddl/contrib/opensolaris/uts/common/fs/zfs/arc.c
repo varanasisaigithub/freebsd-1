580a6d4cf0c5d35ce833a26eb973d0a2309a8ebd
kmacy <kmacy@FreeBSD.org>
2009-11-30 05:36:40 +0000

    arcs_lsize only has two fields - undo breakage

--- sys/cddl/contrib/opensolaris/uts/common/fs/zfs/arc.c
+++ sys/cddl/contrib/opensolaris/uts/common/fs/zfs/arc.c
@@ -1548,9 +1548,7 @@ arc_evict(arc_state_t *state, spa_t *spa, int64_t bytes, boolean_t recycle,
 		list_count = ARC_BUFC_NUMDATALISTS;
 		idx = evict_data_offset;
 	}
-	for (bytes_remaining = 0, i = 0; i < list_count; i++) 
-                bytes_remaining += evicted_state->arcs_lsize[i + offset]; 
-
+	bytes_remaining = evicted_state->arcs_lsize[type];
 	count = 0;
 	
 evict_start:
